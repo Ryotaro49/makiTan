7692095dac89fac9b9a0b35a8466fd7fcf0e6558
Kenji YAMADA <kenzauros@users.noreply.github.com>
2023-11-01 16:44:04 +0900

    react-ts ログイン画面を実装

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [username, setUsername] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [message, setMessage] = useState("");
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!passwordRef.current) {
+        return;
+      }
+      const password = passwordRef.current.value;
+      if (username === "" || password === "") {
+        setMessage("ユーザー名とパスワードを入力してください。");
+        return;
+      }
+      if (username !== password) {
+        setMessage("ユーザー名とパスワードが一致しません。");
+        return;
+      }
+      setMessage("");
+      alert(`${username} さん、ようこそ！`);
+    },
+    [username, passwordRef, setMessage]
+  );
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <div className="box">
+          <input
+            type="text"
+            name="username"
+            placeholder="ユーザー名"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <div id="name"></div>
+        </div>
+        <div className="box">
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="パスワード"
+            ref={passwordRef}
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+          >
+            {showPassword ? "Hide" : "Show"}
+          </button>
+        </div>
+        <div className="box">
+          <div id="message">{message}</div>
+          <div className="box">
+            <button type="submit">ログイン</button>
+          </div>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
