38c2e33a0e5c4f32f6cfc77ab9cbcfbd39c01760
Ryotaro MAKITA <Ryotaro49@users.noreply.github.com>
2023-11-17 13:27:39 +0900

    fix icon style

--- react-ts/src/components/elements/PasswordInput.tsx
+++ react-ts/src/components/elements/PasswordInput.tsx
@@ -8,15 +8,15 @@ export function PasswordInput(props: PasswordInputProps) {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <>
+    <div className="relative inline-flex items-center rounded border">
       <input {...props} type={showPassword ? "text" : "password"} />
       <button
         type="button"
         onClick={() => setShowPassword((prev) => !prev)}
-        className="hover:bg-teal-50"
+        className="absolute right-2 hover:bg-teal-50"
       >
         {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
       </button>{" "}
-    </>
+    </div>
   );
 }
