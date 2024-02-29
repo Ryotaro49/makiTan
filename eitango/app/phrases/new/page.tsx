5bf5f7e852bda819fa010bfdeed39d18763c61ae
Ryotaro MAKITA <Ryotaro49@users.noreply.github.com>
2024-02-29 17:25:41 +0900

    新しいフレーズページを追加

---
+++ eitango/app/phrases/new/page.tsx
@@ -0,0 +1,16 @@
+import Loading from "@/components/Loading";
+import NewPhrase from "@/components/parts/NewPhrase";
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import ErrorBoundary from "next/dist/client/components/error-boundary";
+import Link from "next/link";
+import { Suspense } from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <Suspense fallback={<Loading />}>
+        <NewPhrase />
+      </Suspense>
+    </main>
+  );
+}
