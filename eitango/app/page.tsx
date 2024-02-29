0b1d2d48d5669d952645023c9691d94d3fa8794a
Ryotaro MAKITA <Ryotaro49@users.noreply.github.com>
2024-02-29 17:25:09 +0900

    トップを更新

--- eitango/app/page.tsx
+++ eitango/app/page.tsx
@@ -1,15 +1,21 @@
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid, Stack, TextField, Typography } from "@mui/material";
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      <Link href="/phrases" >
-        <Button variant="outlined" size="large" >単語一覧</Button>
-      </Link>
-      <Link href="/test" >
-        <Button variant="outlined" size="large" >テスト画面</Button>
-      </Link>
-    </main>
+<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '25vh' }}>
+  <Grid container spacing={2} justifyContent="center">
+      <Grid item textAlign="center">
+        <Link href="/phrases">
+          <Button variant="outlined" size="large" sx={{ width: '200px', height: '60px' }}>単語一覧</Button>
+        </Link>
+    </Grid>
+    <Grid item textAlign="center">
+        <Link href="/test">
+          <Button variant="outlined" size="large" sx={{ width: '200px', height: '60px' }}>テスト画面</Button>
+        </Link>
+    </Grid>
+  </Grid>
+</Box>
   );
 }
