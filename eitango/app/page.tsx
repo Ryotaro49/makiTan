a2ca3b47c495510e6b6143c8968904709fd54ef6
Ryotaro MAKITA <Ryotaro49@users.noreply.github.com>
2024-03-02 22:36:12 +0900

    ページのレイアウトを修正

--- eitango/app/page.tsx
+++ eitango/app/page.tsx
@@ -1,21 +1,41 @@
 import { Box, Button, Grid, Stack, TextField, Typography } from "@mui/material";
-import Link from 'next/link';
+import Link from "next/link";
+import TestConfigModal from "./Client/TestConfigModal";
 
 export default function Home() {
   return (
-<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '25vh' }}>
-  <Grid container spacing={2} justifyContent="center">
-      <Grid item textAlign="center">
-        <Link href="/phrases">
-          <Button variant="outlined" size="large" sx={{ width: '200px', height: '60px' }}>単語一覧</Button>
-        </Link>
-    </Grid>
-    <Grid item textAlign="center">
-        <Link href="/test">
-          <Button variant="outlined" size="large" sx={{ width: '200px', height: '60px' }}>テスト画面</Button>
-        </Link>
-    </Grid>
-  </Grid>
-</Box>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "25vh",
+      }}
+    >
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item textAlign="center">
+          <Link href="/phrases">
+            <Button
+              variant="outlined"
+              size="large"
+              sx={{ width: "200px", height: "60px" }}
+            >
+              単語一覧
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item textAlign="center">
+          <Link href="/test">
+            <Button
+              variant="outlined"
+              size="large"
+              sx={{ width: "200px", height: "60px" }}
+            >
+              テスト画面
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
