69c5f06ec5f9e2ead84e5592a686b42b53490f6a
Ryotaro MAKITA <Ryotaro49@users.noreply.github.com>
2024-03-02 22:36:21 +0900

    APIの変更: ノート一覧を取得するAPIのコメントを修正し、新規登録するAPIを追加

--- eitango/app/api/phrases/route.ts
+++ eitango/app/api/phrases/route.ts
@@ -5,13 +5,14 @@ import { NextRequest, NextResponse } from "next/server";
 // 1. 動的レンダリングを強制する
 export const dynamic = "force-dynamic";
 
-// 2. ノート一覧を取得するAPI
+// 2. 一覧を取得するAPI
 export async function GET() {
-  // 3. DBからノート一覧を取得
+  // 3. DBから一覧を取得
   const phrases = await prisma.tango.findMany();
   return NextResponse.json(phrases);
 }
 
+// 新規登録するAPI
 export async function POST(req: NextRequest) {
   const data = await req.json();
   const parcedData = zUpsertPhrase.parse(data);
