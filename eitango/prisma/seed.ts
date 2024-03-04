190a71d712bdf8e588c62b79a917bf0ebbb7312c
Ryotaro MAKITA <Ryotaro49@users.noreply.github.com>
2024-03-04 16:53:23 +0900

    PrismaClientのimport文を修正し、seedデータを追加

--- eitango/prisma/seed.ts
+++ eitango/prisma/seed.ts
@@ -1,6 +1,6 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
   // delete all
@@ -11,47 +11,47 @@ async function main() {
     {
       key: "version",
       value: "13.2.1",
-    }
+    },
   ];
   for (const metadata of metadatas) {
     await prisma.metadata.create({
-      data: metadata
+      data: metadata,
     });
   }
 
   const phrases: Prisma.tangoCreateInput[] = [
     {
-        user_id: 1,
-        phrase: 'example1',
-        meaning: '例文1の意味',
-        category: 'example',
-        registration_date: new Date(),
-        updated_at: new Date(),
-        is_passed: 1,
-      },
-      {
-        user_id: 2,
-        phrase: 'example2',
-        meaning: '例文2の意味',
-        category: 'example',
-        registration_date: new Date(),
-        updated_at: new Date(),
-        is_passed: 0,
-      },
+      user_id: 1,
+      phrase: "example1",
+      meaning: "例文1の意味",
+      category: "example",
+      registration_date: new Date(),
+      updated_at: new Date(),
+      is_passed: true,
+    },
+    {
+      user_id: 2,
+      phrase: "example2",
+      meaning: "例文2の意味",
+      category: "example",
+      registration_date: new Date(),
+      updated_at: new Date(),
+      is_passed: false,
+    },
   ];
   for (const phrase of phrases) {
     await prisma.tango.create({
-      data: phrase
-    })
+      data: phrase,
+    });
   }
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
