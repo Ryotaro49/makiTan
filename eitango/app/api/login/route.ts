23a5ad0966382641d298af7853630e2b6953ea62
Ryotaro49 <ryotaro200049@gmail.com>
2024-06-23 19:15:38 +0900

    トークンを使用してcookieに保存するように修正

--- eitango/app/api/login/route.ts
+++ eitango/app/api/login/route.ts
@@ -1,8 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"; // トークンを生成するためにjsonwebtokenをインポート
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const prisma = new PrismaClient();
+const SECRET_KEY = process.env.SECRET_KEY ?? ""; // 環境変数からSECRET_KEYを取得し、デフォルト値として空文字列を設定
 
 export async function POST(req: NextRequest) {
   try {
@@ -35,11 +40,24 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    // 認証成功
-    return NextResponse.json({
+    // 認証成功 - トークンを生成してクッキーに保存
+    const token = jwt.sign({ userId: user.user_id }, SECRET_KEY, {
+      expiresIn: "1h", // トークンの有効期限を1時間に設定
+    });
+
+    const response = NextResponse.json({
       userId: user.user_id,
       message: "Login successful",
     });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60, // 1時間 (秒単位)
+      path: "/",
+    });
+
+    return response;
   } catch (error) {
     return NextResponse.json(
       { message: "Internal server error" },
