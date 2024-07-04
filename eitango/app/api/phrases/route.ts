9a2d3e43f23b8ea18e96f421c0e7cc7375d435a7
Ryotaro49 <ryotaro200049@gmail.com>
2024-07-04 23:40:22 +0900

    [wip]cookieからuseridを取得する処理を追加

--- eitango/app/api/phrases/route.ts
+++ eitango/app/api/phrases/route.ts
@@ -3,17 +3,47 @@ import { prisma } from "@/globals/db";
 import { listItemTextClasses } from "@mui/material";
 import { tango } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY ?? "";
 
 // 1. 動的レンダリングを強制する
 export const dynamic = "force-dynamic";
 
+// ユーザーIDをトークンからデコードするヘルパー関数
+function getUserIdFromToken(req: NextRequest): number | null {
+  const token = req.cookies.get("token");
+  console.log("token", token);
+  if (!token) return null;
+
+  try {
+    const decoded: any = jwt.verify(token, SECRET_KEY);
+    return parseInt(decoded.userId, 10);
+  } catch (error) {
+    console.error("Invalid token:", error);
+    return null;
+  }
+}
+
 export async function GET(req: NextRequest) {
+  const userId = getUserIdFromToken(req);
+  // const userId = 1;
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
   const params = new URL(req.nextUrl).searchParams;
   const unPassedOnlyChecked = params.get("unPassedOnlyChecked");
   const selectedValue = params.get("selectedValue");
   const category = params.get("category");
 
   const filterOptions = {};
+
+  Object.assign(filterOptions, {
+    where: { user_id: userId },
+  });
 
   if (unPassedOnlyChecked == "true") {
     Object.assign(filterOptions, {
@@ -39,13 +69,17 @@ export async function GET(req: NextRequest) {
 
 // 新規登録するAPI
 export async function POST(req: NextRequest) {
+  const userId = getUserIdFromToken(req);
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
   const data = await req.json();
   const parsedData = zUpsertPhrase.parse(data);
   const phrase = await prisma.tango.create({
     data: {
       phrase: parsedData.phrase,
       meaning: parsedData.meaning,
-      user_id: parsedData.user_id,
+      user_id: userId,
       category: parsedData.category,
       registration_date: new Date(),
       updated_at: new Date(),
