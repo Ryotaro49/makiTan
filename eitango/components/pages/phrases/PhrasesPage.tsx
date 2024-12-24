b29c32cc569e7a0706117f69cd7a39a072d93b1f
Ryotaro MAKITA <Ryotaro49@users.noreply.github.com>
2024-12-24 18:37:10 +0900

    PhrasesPageのスタイルをレスポンシブ対応に修正

--- eitango/components/pages/phrases/PhrasesPage.tsx
+++ eitango/components/pages/phrases/PhrasesPage.tsx
@@ -2,7 +2,13 @@
 
 import Loading from "@/components/Loading";
 import TangoList from "@/components/parts/TangoList";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import Link from "next/link";
 import useSWR from "swr";
 
@@ -13,11 +19,14 @@ export function PhrasesPage() {
     isLoading,
   } = useSWR("/api/phrases", (url) => fetch(url).then((res) => res.json()));
 
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   if (isLoading) {
     return <Loading />;
   }
   return (
-    <Box p={5}>
+    <Box px={isMobile ? 0 : 5} pt={isMobile ? 2 : 5}>
       <Box display={"flex"} justifyContent={"end"} mb={5}>
         {/* 3. Client ComponentsのSuspenseの使用 */}
         <Link href="/phrases/new">
