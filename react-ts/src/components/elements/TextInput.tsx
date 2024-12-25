07bc776b0ededc88a89283fc8780bfb4cddb392c
Ryotaro49 <ryotaro200049@gmail.com>
2024-12-26 04:31:44 +0900

    不要なファイルと設定を削除

--- react-ts/src/components/elements/TextInput.tsx
+++
@@ -1,9 +0,0 @@
-import { InputHTMLAttributes } from "react";
-
-type TextInputProps = InputHTMLAttributes<HTMLInputElement>;
-
-export function TextInput(props: TextInputProps) {
-  return (
-    <input {...props} className="border-teal rounded border bg-slate-50" />
-  );
-}
